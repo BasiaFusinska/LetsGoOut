7dadb7e84106dc7c5989ecb4ec97a397908c7148
BasiaFusinska <barbara.fusinska@gmail.com>
2014-09-13 09:56:18 +0100

    layout format

--- LetsGoOut/App_Start/RouteConfig.cs
+++ LetsGoOut/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace LetsGoOut
@@ -16,7 +12,7 @@ namespace LetsGoOut
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Schedule", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
